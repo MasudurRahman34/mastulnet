cd78f83b00dd9b221413d7f69ed094540ec16469
MasudurRahman34 <mohammadmasud34@gmail.com>
2019-05-12 14:30:46 +0600

    our partners and latest Donations added in index page

--- resources/views/frontend/pages/index.blade.php
+++ resources/views/frontend/pages/index.blade.php
@@ -1,36 +1,34 @@
 @extends('frontend.layouts.master')
 
 @section('content')
+<link href="{{asset('css/owl.carousel.min.css')}}" rel="stylesheet" />
+<div class="header_banner">
+    <div class="container">
+        <h1 class="text-center lgt ">Become a <span class="clr_red typewrite" data-period="500" data-type='[ "Sponsor", "Volunteer", "Member" ]'></span></h1>
+        <span class="wrap"></span>
+    </div>
+</div>
+<!--Header banner section end-->
+<!--Our course section start-->
+<div class="our_courses bg_grey">
+    <div class="container">
+        <div class="row dis_table">
+            @foreach ($courseitem as $courseitem)
+            <div class="dis_cell col-lg-3 col-md-3 col-sm-6 col-xs-6 full_width_599">
+                <div class="our_course_item border_rad_5">
+                    <a href="{!! route('singlecourse.show', $courseitem->slug) !!}">
+                        <img src="{{ asset('images/courseimage/'.$courseitem->imagefile) }}" alt="course">
+                    </a>
+                    <div class="caption">
 
-
-
-        <div class="header_banner">
-            <div class="container">
-                <h1 class="text-center lgt ">Become a <span class="clr_red typewrite" data-period="500" data-type='[ "Sponsor", "Volunteer", "Member" ]'></span></h1>
-                <span class="wrap"></span>
-            </div>
-        </div>
-        <!--Header banner section end-->
-        <!--Our course section start-->
-        <div class="our_courses bg_grey">
-            <div class="container">
-                <div class="row dis_table">
-                    @foreach ($courseitem as $courseitem)
-                    <div class="dis_cell col-lg-3 col-md-3 col-sm-6 col-xs-6 full_width_599">
-                        <div class="our_course_item border_rad_5">
-                                <a href="{!! route('singlecourse.show', $courseitem->slug) !!}">
-                                <img src="{{ asset('images/courseimage/'.$courseitem->imagefile) }}" alt="course">
-                            </a>
-                            <div class="caption">
-
-                                <h3 class="bold font_s_20">{{$courseitem->course_name}}</h3>
-                                <p>{{$courseitem->title}}</p>
-                                <p>{{$courseitem->description}}</p>
-                                <a href="{{ route('singlecourse.show', $courseitem->slug) }}"><i class="icofont-rounded-right"></i> waitng 204 days</a>
-                            </div>
-                        </div>
+                        <h3 class="bold font_s_20">{{$courseitem->course_name}}</h3>
+                        <p>{{$courseitem->title}}</p>
+                        <p>{{$courseitem->description}}</p>
+                        <a href="{{ route('singlecourse.show', $courseitem->slug) }}"><i class="icofont-rounded-right"></i> waitng 204 days</a>
                     </div>
-                    @endforeach
+                </div>
+            </div>
+            @endforeach
                    {{--  <div class="dis_cell col-lg-3 col-md-3 col-sm-6 col-xs-6 full_width_599">
                         <div class="our_course_item border_rad_5">
                             <a href="mobile_app_design.html">
@@ -133,7 +131,7 @@
         <!--Emergency article section start-->
         <div class="emergency_article bg_grey">
             <div class="container">
-                <h1 class="lgt text-center">Our Recent Events</h1>
+                <h1 class="lgt text-center">Recent News & Events</h1>
                 <div class="row">
                     <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 full_width_599">
                         <a href="#">
@@ -172,5 +170,152 @@
             </div>
         </div>
 
-    {{-- expr --}}
-@endsection
+        <div class="emergency_article">
+            <div class="container">
+                <h1 class="lgt text-center">Latest Donations</h1>
+                <div class="row">
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 full_width_599">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white" style="text-align: center;">
+                                    <h3 class="font_s_20">Antory islam (CEO)</h3> 
+                                    <h4 class="font_s_15"> RPAC</h4>
+                                    <p>job details job details job details job details job details job details job details job details</p>
+                                    <p style="color: red">Amount 3000</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 full_width_599">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+
+                                <img src="images/article_1.jpg" alt="img">
+                               <div class="article_details2 bg_white" style="text-align: center;">
+                                    <h3 class="font_s_20">Antory islam (CEO)</h3> 
+                                    <h4 class="font_s_15"> RPAC</h4>
+                                    <p>job details job details job details job details job details job details job details job details</p>
+                                    <p style="color: red">Amount 3000</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-md-4 col-sm-6 col-xs-6 full_width_599">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white" style="text-align: center;">
+                                    <h3 class="font_s_20">Antory islam (CEO)</h3> 
+                                    <h4 class="font_s_15"> RPAC</h4>
+                                    <p>job details job details job details job details job details job details job details job details</p>
+                                    <p style="color: red">Amount 3000</p>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="emergency_article">
+            <div class="container">
+                <h1 class="lgt text-center">Our patners</h1>
+                <div class="owl-carousel owl-theme">
+                    <div class="ml-2 mr-2">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white">
+                                    <h3 class="font_s_20">What is Graphic Design</h3>
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="ml-2 mr-2">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white">
+                                    <h3 class="font_s_20">What is Graphic Design</h3>
+                                   
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                     <div class="ml-2 mr-2">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white">
+                                    <h3 class="font_s_20">What is Graphic Design</h3>
+                                  
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                     <div class="ml-2 mr-2">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white">
+                                    <h3 class="font_s_20">What is Graphic Design</h3>
+                                  
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                     <div class="ml-2 mr-2">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white">
+                                    <h3 class="font_s_20">What is Graphic Design</h3>
+                                   
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                     <div class="ml-2 mr-2">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white">
+                                    <h3 class="font_s_20">What is Graphic Design</h3>
+                                    
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                     <div class="ml-2 mr-2">
+                        <a href="#">
+                            <div class="article2 border_rad_5">
+                                <img src="images/article_1.jpg" alt="img">
+                                <div class="article_details2 bg_white">
+                                    <h3 class="font_s_20">What is Graphic Design</h3>
+                                   
+                                </div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <script src=" {{asset('js/jquery.min.js') }}"></script>
+        <script src="{{ asset('js/owl.carousel.min.js')}}"></script>
+        <script>
+            var owl = $('.owl-carousel');
+            owl.owlCarousel({
+                items:5,
+                loop:true,
+                margin:10,
+                autoplay:true,
+                autoplayTimeout:1000,
+                autoplayHoverPause:true
+            });
+
+        </script>
+        @endsection
